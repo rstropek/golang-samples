49c8ce6b730d66d6e740cfb53d324987d5697793
Rainer <rainer@software-architects.at>
2021-08-11 08:40:31 +0200

    Update C interop to shared library

--- hello-go/c-interop/main.go
+++ hello-go/c-interop/main.go
@@ -1,5 +1,13 @@
 package main
 
+// How to compile C-code
+// gcc -c -Wall -Werror -fpic qps.c
+// gcc -shared -o libqps.so qps.o
+// rm qps.o
+// echo $LD_LIBRARY_PATH
+// export LD_LIBRARY_PATH=$(pwd)
+
+// #cgo LDFLAGS: -L. -lqps
 // #include <stdio.h>
 // #include <stdlib.h>
 // #include "qps.h"
